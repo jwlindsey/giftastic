33c5eea6fb7b505a0507619ea9bc83ce5047c5da
jwlindsey <j.w.lindsey1986@gmail.com>
2016-08-30 17:31:26 -0500

    got the buttons to add

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -10,11 +10,11 @@ var gifsArray = ["dogs", "cats", "mice", "salamanders", "aliens", "humans"];
 //display search function - renders the search parameters into html
 function searchGifs (){
 
-  var animal = $(this).data("animal");
+  var animal = $(this).attr("data-animal");
   var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 //create Ajax call for specific gifs
 $.ajax({
-  URL: queryURL,
+  url: queryURL,
   method: "GET",
   datatype: "json"})
   .done(function(response) {
@@ -25,29 +25,30 @@ $.ajax({
 //generates buttons for anything in the array
 function renderButtons(){
 //deletes gifs prior to adding a new gifs to cut down on repeat buttons
-  $(".gifButtons").remove();
+  $(".gifButtons").empty();
 //loops through array of gifs
-  for (var i = 0; i < gifsArray.length; i++);
+  for (var i = 0; i < gifsArray.length; i++) {
 //generate buttons for each gif in the array
   var a = $("<button>");
   a.addClass("gifsArray");
-  a.attr("data.animal", gifsArray[i]);
+  a.attr("data-animal", gifsArray[i]);
   a.html(gifsArray[i]);
   $(".gifButtons").append(a);
 }
+}
 
 //function for search click
-$("#results").on("click", function(){
+$("#form").on("submit", function(event){
+  event.preventDefault();
 //grab input from search box
-  var gifSearch = $("#searchBox").val().trim();
+  var gifSearch = $("#search-box").val().trim();
 //push from search box into the array
-  gifsArray.push(gif);
+  gifsArray.push(gifSearch);
 //run our renderbuttons function to process through our array
   renderButtons();
-//this line supposedly allows you to hit enter
-  return false;
+
 });
 //display gif information
-$(document).on("click",searchBox);
+$(document).on("click", ".gifsArray", searchGifs);
 
 renderButtons();
